fab430487b4152872e2d94db1ef9bf3579398ada
donexfience <donex6fience@gmail.com>
2024-12-09 18:32:59 +0530

    inti:@server_nestjs:  is ready with basic setup accepting db with endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { Logger, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import databaseConfig from './config/database/database.config';
 import { UserModule } from './infrastructure/modules/user.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
+  controllers: [AppController],
+  providers: [AppService],
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
@@ -12,19 +16,19 @@ import { UserModule } from './infrastructure/modules/user.module';
     }),
     MongooseModule.forRootAsync({
       useFactory: async (configService: ConfigService) => {
-        const logger = new Logger('MongooseModule'); 
+        const logger = new Logger('MongooseModule');
         const uri = configService.get('database.uri');
         const user = configService.get('database.user');
         const pass = configService.get('database.pass');
         const dbName = configService.get('database.dbName');
 
         logger.log(`Connecting to MongoDB at ${uri}`);
-        
-        return { 
-          uri, 
-          user, 
-          pass, 
-          dbName 
+
+        return {
+          uri,
+          user,
+          pass,
+          dbName,
         };
       },
       inject: [ConfigService],
